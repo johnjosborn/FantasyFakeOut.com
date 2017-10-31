43a66b059ab8704ba1698fbde6cfe383cbaa4bbc
John <john@lab627.com>
2017-10-30 21:49:12 -0700

    emailer updates

--- php/fp/sendEmail.php
+++ php/fp/sendEmail.php
@@ -1,71 +1,55 @@
 <?php
 
-    require_once "../../PHPMailer/class.phpmailer.php";
+require_once "../../PHPMailer/class.phpmailer.php";
+
+$emailGoodCount = 0;
+$emailBadCount = 0;
+$emailFail = "";
+$linkString = "";
+
+if (isset($_POST['linkString'])){
+
+    $linkString = $_POST['linkString'];
+    $linkAnchor = "<a href='$linkString'>Breaking News</a>";
+
+}
+
+if (isset($_POST['emailList'])){
+
+    $emailArray = $_POST['emailList'];
+
+    $mail = new PHPMailer();
+    $mail->SMTPDebug = 2;
+    $mail->Timeout = 60;
+    $mail->IsSMTP();   
+    //$mail->Port = 465;                                   // set mailer to use SMTP
+    $mail->Host = "localhost";  // specify main and backup server
+    //$mail->Host = "secure211.inmotionhosting.com";
+    $mail->SMTPAuth = true;     // turn on SMTP authentication
+    $mail->Username = "newsdesk@sportsinsider.vegas";  // SMTP username
+    $mail->Password = "s477Y2CElFrZ"; // SMTP password
     
-    $emailGoodCount = 0;
-    $emailBadCount = 0;
-    $emailFail = "";
-    $linkString = "";
+    $mail->From = "newsdesk@sportsinsider.vegas";
+    $mail->FromName = "Vegas Sports Insider";
 
-    if (isset($_POST['linkString'])){
+    $mail->AddAddress("newsdesk@sportsinsider.vegas");
 
-        $linkString = $_POST['linkString'];
+    $mail->IsHTML(true);                                   
+    
+    $mail->Subject = "Injury Alert";
+    $mail->Body    = $linkAnchor;
+    $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
 
+    foreach ($emailArray as $value) {
+        $mail->AddBCC($value);
     }
 
-    if (isset($_POST['emailList'])){
-
-        $emailArray = $_POST['emailList'];
-
-        foreach ($emailArray as $value) {
-            if(sendMail($value, $linkString)){
-                $emailGoodCount++;
-            } else {
-                $emailBadCount++;
-                $emailFail .= "_" . $value;
-            }
-        }
-    }
-
-    if ($emailBadCount > 0){
-        echo "Sent $emailGoodCount emails. Could not send to: $emailFail";     
-    } else {
-        echo "Sent $emailGoodCount emails.";
-    }
-
-    //get posted list
-
-
-    function sendmail($email1, $linkString){
-
-        $mail = new PHPMailer();
-        
-        $mail->IsSMTP();                                      // set mailer to use SMTP
-        $mail->Host = "localhost";  // specify main and backup server
-        $mail->SMTPAuth = true;     // turn on SMTP authentication
-        $mail->Username = "newsdesk@sportsinsider.vegas";  // SMTP username
-        $mail->Password = "s477Y2CElFrZ"; // SMTP password
-        
-        $mail->From = "newsdesk@sportsinsider.vegas";
-        $mail->FromName = "Vegas Sports Insider";
-        $mail->AddAddress($email1);
-        //$mail->AddReplyTo("newsdesk@sportsinsider.vegas", "Vegas Sports Insider");
-        
-        //$mail->WordWrap = 50;                                 // set word wrap to 50 characters
-        //$mail->AddAttachment("/var/tmp/file.tar.gz");         // add attachments
-        //$mail->AddAttachment("/tmp/image.jpg", "new.jpg");    // optional name
-        $mail->IsHTML(true);                                    // set email format to HTML
-        
-        $mail->Subject = "Injury Alert";
-        $mail->Body    = $linkString;
-        $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
-
-        if(!$mail->Send())
-        {
-            echo $mail->ErrorInfo;
-            return false;
-        } 
-        return true;
-    }
+    if(!$mail->Send())
+    {
+        echo $mail->ErrorInfo;
+        return false;
+    } 
+    echo "Mail Sent";
+}
 
 ?>
